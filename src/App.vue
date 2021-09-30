bf2191b28dd2e4a5122a12cbf44e94e0b2b05167
Bitcoin Please <support@bchplease.org>
2021-09-30 04:22:59 -0400

    Add application overlays.

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,27 @@
     <router-view />
 
     <Footer />
+
+    <Profile class="hidden" />
+
+    <Modal class="hidden" />
+
+    <Notif class="hidden" />
 </template>
 
 <script>
 /* Import components. */
 import Footer from '@/components/Footer.vue'
+import Profile from '@/components/Profile.vue'
+import Modal from '@/components/Modal.vue'
+import Notif from '@/components/Notif.vue'
 
 export default {
     components: {
         Footer,
+        Profile,
+        Modal,
+        Notif,
     }
 }
 </script>
